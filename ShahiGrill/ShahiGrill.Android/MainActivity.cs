511d25bb9d996e074983bf576c0fc42655901759
Vincent Akena <vakena@outlook.com>
2019-05-20 13:58:07 +1000

    Android logo and name update

--- ShahiGrill/ShahiGrill.Android/MainActivity.cs
+++ ShahiGrill/ShahiGrill.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ShahiGrill.Droid
 {
-    [Activity(Label = "ShahiGrill", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Shahi Grill", Icon = "@drawable/appLogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
