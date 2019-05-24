c7088b004f49e915ddb0b18b92afa72734b49ee0
Vincent Akena <vakena@outlook.com>
2019-05-24 16:36:30 +1000

    core functionality for menu, reservation and specials have been completed

--- ShahiGrill/ShahiGrill/Views/Reservation.xaml.cs
+++ ShahiGrill/ShahiGrill/Views/Reservation.xaml.cs
@@ -16,5 +16,10 @@ namespace ShahiGrill.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success","Reservation has been sent!","OK");
+        }
     }
 }
