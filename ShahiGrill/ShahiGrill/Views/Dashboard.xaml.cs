b43079d60721c4cd57f62e6c7ca850684b281fe5
Vincent Akena <vakena@outlook.com>
2019-05-24 14:12:05 +1000

    Dashboard items and children elements implemented

--- ShahiGrill/ShahiGrill/Views/Dashboard.xaml.cs
+++ ShahiGrill/ShahiGrill/Views/Dashboard.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using ShahiGrill.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,54 @@ namespace ShahiGrill.Views
         public Dashboard()
         {
             InitializeComponent();
+        }
+
+        private async void TapGestureRecognizerM_TappedAsync(object sender, EventArgs e)
+        {
+            int tapCount = 0;
+            tapCount++;
+            var imageSender = (Image)sender;
+            // watch the monkey go from color to black&white!
+            if (tapCount == 1)
+            {
+                await Navigation.PushAsync(new Menu());
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Navigation Failed", "OK");
+            }
+        }
+
+        private async void TapGestureRecognizerR_TappedAsync(object sender, EventArgs e)
+        {
+            int tapCount = 0;
+            tapCount++;
+            var imageSender = (Image)sender;
+            // watch the monkey go from color to black&white!
+            if (tapCount == 1)
+            {
+                await Navigation.PushAsync(new Reservation());
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Navigation Failed", "OK");
+            }
+        }
+
+        private async void TapGestureRecognizerS_TappedAsync(object sender, EventArgs e)
+        {
+            int tapCount = 0;
+            tapCount++;
+            var imageSender = (Image)sender;
+            // watch the monkey go from color to black&white!
+            if (tapCount == 1)
+            {
+                await Navigation.PushAsync(new Specials());
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Navigation Failed", "OK");
+            }
         }
     }
 }
---
+++ ShahiGrill/ShahiGrill/Views/Reservation.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShahiGrill.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Reservation : ContentPage
+    {
+        public Reservation()
+        {
+            InitializeComponent();
+        }
+    }
+}
