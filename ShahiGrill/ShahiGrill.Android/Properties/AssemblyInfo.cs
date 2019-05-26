ad3c2f151fe43d4281a3589afb4a072d0beabd74
Vincent Akena <vakena@outlook.com>
2019-05-26 12:22:36 +1000

    Android manifest edits for publication

--- ShahiGrill/ShahiGrill.Android/Properties/AssemblyInfo.cs
+++ ShahiGrill/ShahiGrill.Android/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using Android.App;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShahiGrill.Android")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Shahi Grill, a South American restaurant owned by Rob Vartanian in Sydney.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stay Professional")]
 [assembly: AssemblyProduct("ShahiGrill.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("Stay Professional™")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
